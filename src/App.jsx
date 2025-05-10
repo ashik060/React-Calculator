e168a1bbb34d1cada9d176f28ecf80c9827020a0
ashik060 <26172365+ashik060@users.noreply.github.com>
2025-05-10 22:04:26 +0600

    Wrapper.jsx Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Wrapper from './components/Wrapper';
 
 const App = () => {
   return (
     <div>
-      
+      <Wrapper>
+        <h1>Calculator</h1>
+      </Wrapper>
     </div>
   );
 };
