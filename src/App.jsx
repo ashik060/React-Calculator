1ad260c6987cfa13487cdc0260e820f1f297fead
ashik060 <26172365+ashik060@users.noreply.github.com>
2025-05-10 23:10:27 +0600

    ButtonBox and Button file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import Wrapper from './components/Wrapper';
+import Screen from './components/Screen';
+import ButtonBox from './components/ButtonBox';
+import Button from './components/Button';
+
+
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  ["7", "8", "9", "X"],
+  ["4", "5", "6", "+"],
+  ["1", "2", "3", "-"],
+  ["0", ".", "="],
+];
 
 const App = () => {
   return (
     <div>
       <Wrapper>
-        <h1>Calculator</h1>
+        <Screen/>
+        <ButtonBox>
+          {btnValues.flat().map((btn, i) => (
+            <Button 
+            value = {btn}
+            key = {i}
+            />
+          ))}
+        </ButtonBox>
       </Wrapper>
     </div>
   );
---
+++ src/components/Screen.jsx
@@ -0,0 +1,8 @@
+
+const Screen = () => {
+    return (
+        <div className="screen">015482</div>
+    );
+};
+
+export default Screen;
